de02f8a12a3b4cb88aad72c4ecd4550170fe9722
MentalSep <chahidjava@hotmail.com>
2024-02-05 12:39:34 +0100

    add task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,3 +7,23 @@ from api.v1.views import app_views
 def status():
     """ returns status """
     return {"status": "OK"}
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """ returns stats """
+    from models import storage
+    from models.state import State
+    from models.city import City
+    from models.amenity import Amenity
+    from models.place import Place
+    from models.review import Review
+    from models.user import User
+    return {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
