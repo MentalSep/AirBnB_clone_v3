d7cd8eb9c9dfedc9a815b2eaede344700069a362
MentalSep <chahidjava@hotmail.com>
2024-02-06 00:53:26 +0100

    add task 15

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -6,6 +6,8 @@ from models.place import Place
 from api.v1.views import app_views
 from models.city import City
 from models.user import User
+from models.state import State
+from models.amenity import Amenity
 
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'],
@@ -74,3 +76,40 @@ def update_place(place_id):
             setattr(place, key, value)
     storage.save()
     return jsonify(place.to_dict())
+
+
+@app_views.route('/places_search', methods=['POST'], strict_slashes=False)
+def search_places():
+    """ Retrieves the list of all Place objects depending of the JSON
+    in the body of the request """
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+    cities = request.get_json().get('cities', [])
+    states = request.get_json().get('states', [])
+    amenities = request.get_json().get('amenities', [])
+
+    if not cities and not states and not amenities:
+        places = storage.all(Place).values()
+        return jsonify([place.to_dict() for place in places])
+
+    places = []
+    if cities:
+        for city_id in cities:
+            city = storage.get(City, city_id)
+            if city is not None:
+                places.extend(city.places)
+
+    if states:
+        for state_id in states:
+            state = storage.get(State, state_id)
+            if state is not None:
+                for city in state.cities:
+                    places.extend(city.places)
+
+    if amenities:
+        for place in places:
+            place_amenities = {amenity.id for amenity in place.amenities}
+            if not place_amenities.issuperset(amenities):
+                places.remove(place)
+
+    return jsonify([place.to_dict() for place in places])
